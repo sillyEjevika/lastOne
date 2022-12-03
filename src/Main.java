34d72c16c7cb4b3c692f29064d6d49c68ede5f52
sillyEjevika <Doroteia_Vika@mail.ru>
2022-12-04 00:57:49 +0300

    Задание 4 (задача 1)

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задача 1
+        System.out.println("Задача №1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1){
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        }
+    }
